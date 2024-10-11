04ba1879defb2c49bde91e3029a433b19d6cde15
Safalgautam3636 <seeram3636@gmail.com>
2024-10-11 19:25:50 -0400

    Initial commit

---
+++ iOS-CareerScrapeTests/iOS_CareerScrapeTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_CareerScrapeTests.swift
+//  iOS-CareerScrapeTests
+//
+//  Created by Safal Gautam on 10/11/24.
+//
+
+import Testing
+@testable import iOS_CareerScrape
+
+struct iOS_CareerScrapeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
