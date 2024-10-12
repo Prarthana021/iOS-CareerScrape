4ed9ca62cb7195b854e3b4b687f1f19cc7ba911d
Safalgautam3636 <seeram3636@gmail.com>
2024-10-12 13:21:24 -0400

    add todo

---
+++ iOS-CareerScrape/todo.swift
@@ -0,0 +1,8 @@
+// pages
+// login                -- also oAuth options --forgot password
+// signup
+// jobs page
+// users connect options
+// backend- docker, aws, if possible kubernetes
+// follow best practices
+
