95ab66c2cc58dc1918b4c8e95396e46491eedf4b
Safalgautam3636 <seeram3636@gmail.com>
2024-11-30 01:46:51 -0500

    Add auth service class

---
+++ iOS-CareerScrape/Services/AuthService.swift
@@ -0,0 +1,91 @@
+//
+//  AuthService.swift
+//  iOS-CareerScrape
+//
+//  Created by Safal Gautam on 11/28/24.
+//
+
+import Foundation
+
+class AuthService{
+    static let shared = AuthService()
+    private init(){}
+    private let baseURL = "https://api.careerscrape.com/users"
+    
+    private let tokenKey = "jwtToken"
+    func login(username:String, password:String, completion: @escaping (Result<Void,Error>)->Void){
+        guard let url = URL(string: "\(baseURL)/login") else {return}
+        
+        var request = URLRequest(url: url)
+        request.httpMethod="POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let body: [String:String] = [ "username": username,
+                                      "password": password
+        ]
+        request.httpBody = try? JSONSerialization.data(withJSONObject: body,options: [])
+        URLSession.shared.dataTask(with: request){
+            data, response, error in
+            if let error = error{
+                completion(.failure(error))
+                return
+            }
+            
+            guard let data = data,
+                  let json  = try? JSONSerialization.jsonObject(with: data) as? [String:Any],
+                  let token = json["token"] as? String else {
+                completion(.failure(NSError(domain:"Invalid Response", code:0, userInfo: nil)))
+                return
+            }
+            UserDefaults.standard.set(token, forKey: self.tokenKey)
+            completion(.success(()))
+            
+        }.resume()
+        
+    }
+    
+    func signup(username:String, email: String, password: String, isAdmin: Bool?, completion: @escaping(Result<Void, Error>)-> Void){
+        guard let url = URL(string: "\(baseURL)/signup") else {return}
+        
+        var request=URLRequest(url: url)
+        request.httpMethod="POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        var body: [String:Any] = [
+            "username":username,
+            "email":email,
+            "password":password,
+        ]
+        if let isAdmin = isAdmin {
+            body["isAdmin"] = isAdmin
+        }
+        
+        request.httpBody=try? JSONSerialization.data(withJSONObject: body, options: [])
+        
+        URLSession.shared.dataTask(with: request){ data, response, error in
+            
+            if let error = error{
+                completion(.failure(error))
+                return
+            }
+            
+            guard let data = data,
+                  let json = try? JSONSerialization.jsonObject(with: data) as?  [String:Any] else {
+                completion(.failure(NSError(domain: "Invalid Response", code:0, userInfo: nil)))
+                return
+            }
+            
+            if let token = json["token"] as? String {
+                UserDefaults.standard.set(token, forKey: self.tokenKey)
+            }
+            
+            completion(.success(()))
+            
+        }.resume()
+        
+    }
+    func logout(completion: @escaping(Result<Void, Error>)-> Void){
+        UserDefaults.standard.removeObject(forKey: self.tokenKey)
+        completion(.success(()))
+    }
+}
