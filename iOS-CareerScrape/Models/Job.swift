73d64c6d9c50cc9c0dcde2ff04c0495673d0647d
Safalgautam3636 <seeram3636@gmail.com>
2024-11-30 23:04:47 -0500

    Add Job Model and Job Service

--- iOS-CareerScrape/Models/Job.swift
+++ iOS-CareerScrape/Models/Job.swift
@@ -4,3 +4,37 @@
 //
 //  Created by Safal Gautam on 11/28/24.
 //
+import Foundation
+
+struct Job: Identifiable, Codable{
+    var id:String
+    var jobTitle: String
+    var jobLink: String
+    var jobLocation: String
+    var companyName: String
+    var jobType: String
+    var companyDomain: String
+    var jobLevel: String
+    var companyLink:String
+    var description: String
+    var pulledDate: String
+    var exactDate: String
+    var jobPosted: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case jobTitle = "job_title"
+        case jobLink = "job_link"
+        case jobLocation = "job_location"
+        case companyName = "company_name"
+        case jobType = "job_type"
+        case companyDomain = "company_domain"
+        case jobLevel = "job_level"
+        case companyLink = "company_link"
+        case description
+        case pulledDate = "pulled_date"
+        case exactDate = "exact_date"
+        case jobPosted = "job_posted"
+    }
+}
+
--- iOS-CareerScrape/Services/JobService.swift
+++ iOS-CareerScrape/Services/JobService.swift
@@ -5,3 +5,55 @@
 //  Created by Safal Gautam on 11/28/24.
 //
 
+import Foundation
+
+class JobService{
+    static let shared=JobService()
+    private init (){}
+    
+    private let baseURL = "https://api.careerscrape.com/jobs"
+    
+    func fetchJobs(completion: @escaping (Result<[Job], Error>) -> Void){
+        let url = URL(string: "\(baseURL)/jobs")!
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            if let error=error{
+                completion(.failure(error))
+                return
+            }
+            guard let data = data else{
+                completion(.failure(NSError(domain:"No data", code: 0, userInfo: nil)))
+                return
+            }
+            
+            do{
+                let decoder = JSONDecoder()
+                struct JobResponse: Decodable {
+                    let jobs: [Job]
+                }
+                
+                let jobResponse = try decoder.decode(JobResponse.self,from: data)
+                completion(.success(jobResponse.jobs))
+            }
+            catch{
+                completion(.failure(error))
+            }
+        }
+        task.resume()
+    }
+    func fetchJobWithId(){
+        
+    }
+    func deleteJobs(){
+        
+    }
+    func createJob(){
+        
+    }
+    func updateJob(){
+        
+    }
+    
+    
+}
