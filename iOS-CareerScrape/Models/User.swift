4444be2191ed06f39b64e77d6c94f54dbb54ed7f
Safalgautam3636 <seeram3636@gmail.com>
2024-12-09 02:08:07 -0500

    Update User model

--- iOS-CareerScrape/Models/User.swift
+++ iOS-CareerScrape/Models/User.swift
@@ -5,9 +5,25 @@
 //  Created by Safal Gautam on 11/28/24.
 //
 
-struct User: Codable{
+struct User: Identifiable, Codable {
     let id: String
-    
+    let username: String
+    let email: String
+    let followers: [String]
+    let following: [String]
+    let interestedJobs: [String]
+    let isAdmin: Bool
+
+    // Map JSON fields to the `User` properties
+    enum CodingKeys: String, CodingKey {
+        case id
+        case username
+        case email
+        case followers
+        case following
+        case interestedJobs
+        case isAdmin
+    }
 }
 
 struct AuthUser: Codable{
