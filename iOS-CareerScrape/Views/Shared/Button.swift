725f85afe1784ec4af8bb2eb3faddab9afc2fc22
Safalgautam3636 <seeram3636@gmail.com>
2024-12-11 22:09:17 -0500

    Create a generic button

--- iOS-CareerScrape/Views/Shared/Button.swift
+++ iOS-CareerScrape/Views/Shared/Button.swift
@@ -5,3 +5,23 @@
 //  Created by Safal Gautam on 12/7/24.
 //
 
+import SwiftUI
+
+struct CustomButton: View{
+    let title: String
+    let backgroundColor: Color
+    let textColor: Color
+    let action: ()-> Void
+    
+    var body: some View{
+        Button(action: action){
+            Text(title)
+                .font(.headline)
+                .foregroundColor(textColor)
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(backgroundColor)
+                .cornerRadius(8)
+        }
+    }
+}
