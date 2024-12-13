64336ed53485a0947ddf5c037be4315a9ed5deca
Safalgautam3636 <seeram3636@gmail.com>
2024-12-13 02:43:32 -0500

    Fetch all user from the server service

--- iOS-CareerScrape/ViewModels/UserViewModel.swift
+++ iOS-CareerScrape/ViewModels/UserViewModel.swift
@@ -8,6 +8,7 @@ import Foundation
 
 class UserViewModel: ObservableObject{
     @Published var user: User?
+    @Published var users: [User]?
     @Published var error: Error?
     @Published var isLoading: Bool = false
     private let userService = UserService.shared
@@ -24,6 +25,25 @@ class UserViewModel: ObservableObject{
                     self?.error = error
                 case .success(let user):
                     self?.user = user
+                }
+            }
+        }
+    }
+    
+    func fetchAllUsers(){
+        isLoading = true
+        error = nil
+        
+        userService.fetchAllUsers{
+            [weak self] result in
+            
+            DispatchQueue.main.async{
+                self?.isLoading = false
+                switch result {
+                case .failure(let error):
+                    self?.error = error
+                case .success(let users):
+                    self?.users = users
                 }
             }
         }
